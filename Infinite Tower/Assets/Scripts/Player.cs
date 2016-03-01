008ded795f0f3ee00ee0e1cc8793acaf96ebfd44
Christopher Lagerhult <Christopher Lagerhult>
2016-03-01 17:52:11 +0100

    Fixed lag

--- Infinite Tower/Assets/Scripts/Player.cs
+++ Infinite Tower/Assets/Scripts/Player.cs
@@ -4,12 +4,13 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
-    public float jumpForce = 20f;
+    public float jumpForce = 100f;
     public float movementSpeed = 10f;
 
+    private float _movementSpeed { get { return this.movementSpeed * 0.2f; } }
     private bool _onGround = false;
-    private bool _isMovingRight = true;
-    private Vector2 _velocity;
+    private int _isMovingRight = 1;
+    //private Vector2 _velocity;
     private bool _jumpTriggered = false;
     private Rigidbody2D _playerRigidBody2D;
     private Transform _groundCheck, _wallCheckRight, _wallCheckLeft;
@@ -22,44 +23,32 @@ public class Player : MonoBehaviour
         _wallCheckRight = transform.Find("WallCheckRight");
         _wallCheckLeft = transform.Find("WallCheckLeft");
         _playerRigidBody2D = GetComponent<Rigidbody2D>();
-        _velocity = new Vector2(movementSpeed, 0);
+        //_velocity = new Vector2(movementSpeed, 0);
     }
-    public void Update(){
-        if (!_jumpTriggered && _onGround) {
+    public void Update() {
+        if (!_jumpTriggered && _onGround)
             _jumpTriggered = Input.GetMouseButton(0);//Input.GetKeyDown(KeyCode.Space);//
-            if (_jumpTriggered)
-                Debug.Log("Jump triggered");
-        }
-        //Alternate code for touchscreen
-        /*var fingerCount = 0;
-        for (var touch : Touch in Input.touches) {
-            if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled)
-                fingerCount++;
-        }
-        if (fingerCount > 0)
-            print("User has " + fingerCount + " finger(s) touching the screen");*/
     }
     public void FixedUpdate() {
+        //_velocity.x = _movementSpeed;
         if (_jumpTriggered) {
-            Jump();
             _onGround = false;
             _jumpTriggered = false;
-            Debug.Log("Jump removed");
+            Jump();
         }
         else
             _onGround = Physics2D.OverlapCircle(_groundCheck.position, collisionRadius, whatIsGround);
+            //_onGround = Physics2D.CircleCast(_groundCheck.position, collisionRadius, whatIsGround);
 
-        var isCollidingWall = _isMovingRight ? Physics2D.OverlapCircle(_wallCheckRight.position, collisionRadius, whatIsWall) : Physics2D.OverlapCircle(_wallCheckLeft.position, collisionRadius, whatIsWall);
-        if (isCollidingWall) {
-            _velocity.x = -_velocity.x;
-            _isMovingRight = !_isMovingRight;
-        }
+            var isCollidingWall = (_isMovingRight == 1) ? Physics2D.OverlapCircle(_wallCheckRight.position, collisionRadius, whatIsWall) : Physics2D.OverlapCircle(_wallCheckLeft.position, collisionRadius, whatIsWall);
+        if (isCollidingWall)
+            _isMovingRight *= -1;
         Move();
     }
     private void Jump() {
         _playerRigidBody2D.AddForce(new Vector2(0, jumpForce));
     }
     private void Move() {
-        transform.Translate(_velocity * movementSpeed * Time.deltaTime);
+        _playerRigidBody2D.velocity = new Vector2(movementSpeed * _isMovingRight, _playerRigidBody2D.velocity.y);
     }
 }
